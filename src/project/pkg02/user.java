1a5bb5936314997eec4f4372acfdf5d8a71d4dfb
mohamedthasneem <32462188+mohamedthasneem@users.noreply.github.com>
2018-07-16 18:34:57 +0530

    user class added

---
+++ src/project/pkg02/user.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.pkg02;
+
+/**
+ *
+ * @author suthesana
+ */
+public class user {
+private int input;
+    public user() {
+    }
+
+    public user(int input) {
+        this.input = input;
+    }
+
+    public int getInput() {
+        return input;
+    }
+
+    public void setInput(int input) {
+        this.input = input;
+    }
+    
+    
+}
+    
+    
