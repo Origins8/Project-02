602484f03abdd4e89c5d0cc46976d510d9dc7571
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-07-16 22:10:39 +0530

    Classes from karshan and sutheesana are move to specific packages

--- src/User/user.java
+++ src/User/user.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package project.pkg02;
+package User;
 
-/**
- *
- * @author suthesana
- */
 public class user {
-private int input;
+
+    private int input;
+    
     public user() {
     }
 
